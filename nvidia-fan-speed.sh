ec1e59efee3e1afcc84898a52caf9619a5bb8923
Daniel Speck <daniel@dspeck.de>
2017-04-19 16:49:30 +0200

    initial shell commit

---
+++ nvidia-fan-speed.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+
+function read_gpu_temp {
+    var_gpu_temp = $(nvidia-settings -q GPUCoreTemp | grep -E "GPUCoreTemp" | grep -E "gpu:0" | grep -E "[0-9]{1,3}*.$" --only-matching | grep -E "[0-9]*" --only-matching)
+}
+
+function read_fan_speed {
+    var_fan_speed = $(nvidia-settings -q GPUCurrentFanSpeed | grep -E "GPUCurrentFanSpeed" | grep -E "fan:0" | grep -E "[0-9]{1,3}*.$" --only-matching | grep -E "[0-9]*" --only-matching)
+}
+
