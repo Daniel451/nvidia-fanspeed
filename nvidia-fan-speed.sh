843051988fbfedfd6423255d2ab8c3c32e95a1f3
12speck <12speck@ws16.lan>
2017-04-19 18:07:53 +0200

    working

--- nvidia-fan-speed.sh
+++ nvidia-fan-speed.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 read_gpu_temp() {
     temp=$(nvidia-settings -q GPUCoreTemp | grep -E "GPUCoreTemp" | grep -E "gpu:0" | grep -E "[0-9]{1,3}*.$" --only-matching | grep -E "[0-9]*" --only-matching)
     echo ${temp}
@@ -10,48 +11,55 @@ read_fan_speed() {
     echo ${speed}
 }
 
+timestamp() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')]"
+}
+
 set_fan_speed() {
+    dt=$(timestamp)
     # set new fan speed
     nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=${1}"
-    # wait 5 seconds for fan to reach new speed level
-    sleep 5s
+    echo "${dt} setting fan speed to ${1}%"
+    echo "sleeping 10s to wait for fan reaching new speed level..."
+    # wait 10 seconds for fan to reach new speed level
+    sleep 10s
 }
 
 print_info() {
-    echo "temp is ${1}"
-    echo "speed is ${2}"
+    dt=$(timestamp)
+    echo "${dt} temp is ${1}"
+    echo "${dt} speed is ${2}"
 }
 
 adjust_fan_speed() {
     # get current speed and temp
     temp=$(read_gpu_temp)
     speed=$(read_fan_speed)
+
+    # comment or uncomment debug printing here
+    #print_info $temp $speed
     
     # for low temperature
     if (($temp < 50))
     then
-        print_info $temp $speed
         if (($speed < 30 || $speed > 35))
         then
             set_fan_speed 30 
         fi        
     elif (($temp >= 50 && $temp < 60))
     then
-        print_info $temp $speed
         if (($speed < 45 || $speed > 55))
         then
             set_fan_speed 50 
         fi        
-    elif (($temp >= 60 && $temp < 74))
+    elif (($temp >= 60 && $temp < 73))
     then
-        print_info $temp $speed
         if (($speed < 70 || $speed > 80))
         then
             set_fan_speed 75 
         fi        
-    elif (($temp >= 75))
+    elif (($temp >= 73))
     then
-        print_info $temp $speed
         if (($speed < 95))
         then
             set_fan_speed 100
@@ -60,6 +68,7 @@ adjust_fan_speed() {
 }
 
 
+# main loop
 while true
 do
     adjust_fan_speed
